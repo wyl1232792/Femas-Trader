aab4c8b35521b64f84d10f0de81d8223fc102dae
wyl1232792 <wyl1232792@hotmail.com>
2016-02-15 17:32:02 +0800

    add a test of http client

--- src/GeneralManager/src/cn/wyl1232792/fdp/FinancialDataPlatform.java
+++ src/GeneralManager/src/cn/wyl1232792/fdp/FinancialDataPlatform.java
@@ -1,9 +1,28 @@
 package cn.wyl1232792.fdp;
+import java.nio.charset.Charset;
 
+import org.eclipse.jetty.client.HttpClient;
 public class FinancialDataPlatform {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		HttpClient httpClient = new HttpClient();
+		try {
+			httpClient.start();
+			httpClient.newRequest("http://www.baidu.com")
+				.onResponseContent((response, buffer) -> {
+					
+					System.out.println(Charset.forName("utf-8").decode(buffer).toString());
+				})
+				.send((result) -> {
+					if (result.isFailed())
+						System.out.println("failed");
+				});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("running");
 	}
 
 }
